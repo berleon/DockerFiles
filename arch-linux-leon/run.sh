f8ac3640caa5dada38ac674c849ff52462401c47
berleon <github@leon-sixt.de>
2016-02-22 19:09:29 +0100

    add run script for docker with cuda

---
+++ arch-linux-leon/run.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+
+docker run -ti \
+    --device /dev/nvidia0:/dev/nvidia0 \
+    --device /dev/nvidiactl:/dev/nvidiactl \
+    --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+    -v /home:/home -v /mnt:/mnt \
+    --publish 9997:9997 \
+    --name leon_gan_container leon/gan  /bin/zsh
